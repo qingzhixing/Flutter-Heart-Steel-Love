0fda045458c4e9afabc553fd722f520907be7168
qingzhixing <qzsqqmail@qq.com>
2025-08-06 18:14:48 +0800

    钢！

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,32 @@
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+var appThemeData = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  useMaterial3: true,
+);
+
+void heartSteelButtonPressed() async {
+  // 三选一播放文件
+  var sfxId = Random().nextInt(3) + 1;
+  var player = AudioPlayer();
+  // Set the release mode to keep the source after playback has completed.
+  player.setReleaseMode(ReleaseMode.stop);
+  await player.setSource(AssetSource('SFX/Heartsteel_trigger_SFX_$sfxId.mp3'));
+
+  await player.resume();
+}
 
 void main() {
   runApp(
     MaterialApp(
-      title: "庞然吞噬之我要叠钢钢钢钢",
+      title: "庞然吞噬 之 我要叠钢钢钢钢",
       home: const MainPage(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: appThemeData,
+      debugShowCheckedModeBanner: false,
     ),
   );
 }
@@ -20,11 +39,53 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Center(
-          child: Text('庞然吞噬之我要叠钢钢钢钢'),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 10,
+            children: [
+              Text(
+                '庞然吞噬 之 我要 叠！钢！',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Microsoft YaHei",
+                ),
+                textAlign: TextAlign.center,
+              ),
+              Icon(
+                Icons.heart_broken,
+              ),
+            ],
+          ),
         ),
       ),
-      body: const Center(
-        child: Text('Hello World!'),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 50,
+            bottom: 50,
+            right: 50,
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Ink(
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage('assets/icon/HeartSteel.jpg'),
+                        fit: BoxFit.fill),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(50),
+                    onTap: heartSteelButtonPressed,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
