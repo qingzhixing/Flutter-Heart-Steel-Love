bc05bfebd72c61aaa4ce0033529812ddfcd62ce3
qingzhixing <qzsqqmail@qq.com>
2025-10-02 16:43:39 +0800

    定时叠钢

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-
+import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
@@ -7,17 +7,6 @@ var appThemeData = ThemeData(
   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
   useMaterial3: true,
 );
-
-void heartSteelButtonPressed() async {
-  // 三选一播放文件
-  var sfxId = Random().nextInt(3) + 1;
-  var player = AudioPlayer();
-  // Set the release mode to keep the source after playback has completed.
-  player.setReleaseMode(ReleaseMode.stop);
-  await player.setSource(AssetSource('SFX/Heartsteel_trigger_SFX_$sfxId.mp3'));
-
-  await player.resume();
-}
 
 void main() {
   runApp(
@@ -30,8 +19,73 @@ void main() {
   );
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late Timer chargeTimer;
+  // 心之钢层数
+  int _heartSteelStack = 0;
+  // 心之钢充能
+  int _heartSteelCharge = 0;
+
+  void _onCharged() {
+    if (_heartSteelCharge < 3) {
+      setState(() {
+        _heartSteelCharge++;
+      });
+      // SFX
+      var player = AudioPlayer();
+      player.setReleaseMode(ReleaseMode.stop);
+
+      // 第三层的充能特效不同
+      if (_heartSteelCharge == 3) {
+        var sfxId = Random().nextInt(3) + 1;
+        player
+            .setSource(AssetSource('SFX/Heartsteel_3rd_stack_SFX_$sfxId.mp3'));
+      } else {
+        var sfxId = Random().nextInt(4) + 1;
+        player.setSource(AssetSource('SFX/Heartsteel_stack_SFX_$sfxId.mp3'));
+        // 此时还能继续充能
+        chargeTimer = Timer(const Duration(milliseconds: 1000), _onCharged);
+      }
+
+      player.setVolume(0.5);
+      player.resume();
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    chargeTimer = Timer(const Duration(milliseconds: 1000), _onCharged);
+  }
+
+  void _onHeartSteelPressed() {
+    if (_heartSteelCharge < 3) {
+      return;
+    }
+
+    setState(() {
+      _heartSteelStack += Random().nextInt(65);
+      _heartSteelCharge = 0;
+    });
+
+    // 三选一播放文件
+    var sfxId = Random().nextInt(3) + 1;
+    var player = AudioPlayer();
+    player.setReleaseMode(ReleaseMode.stop);
+    player.setSource(AssetSource('SFX/Heartsteel_trigger_SFX_$sfxId.mp3'));
+    player.setVolume(0.3);
+    player.resume();
+
+    // 恢复充能
+    chargeTimer = Timer(const Duration(milliseconds: 1000), _onCharged);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +98,7 @@ class MainPage extends StatelessWidget {
             spacing: 10,
             children: [
               Text(
-                '庞然吞噬 之 我要 叠！钢！',
+                '我要 叠！钢！',
                 style: TextStyle(
                   fontSize: 25,
                   fontWeight: FontWeight.bold,
@@ -78,7 +132,7 @@ class MainPage extends StatelessWidget {
                   ),
                   child: InkWell(
                     borderRadius: BorderRadius.circular(50),
-                    onTap: heartSteelButtonPressed,
+                    onTap: _onHeartSteelPressed,
                   ),
                 ),
               ),
